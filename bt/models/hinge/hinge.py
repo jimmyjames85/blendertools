5a637d9470ac9cd557db9a39f243972620c6979d
jimmyjames <james.tappe@sendgrid.com>
2018-02-25 13:25:42 -0800

    saving configs

---
+++ bt/models/hinge/hinge.py
@@ -0,0 +1,64 @@
+from bt import mesh, object
+from bt.common import XAXIS, YAXIS, ZAXIS
+from math import sin, cos, radians
+import bpy
+
+
+#
+# Just so you know, You should always size your holes with proper
+# clearance in your models. A 20mm hole for a 20mm part is a "size
+# on size" press fit. Adding .02mm should be a snug press fit,
+# 0.05mm would be a tight slip, 0.1mm a snug slip, and 0.15mm a
+# free slip, and 0.4mm would give you a clearance fit for a
+# bolt. Of course these dimensions vary somewhat as a percentage of
+# the size of the object and the hole - (bigger needs slightly
+# more), but generally a good rule of thumb.
+#
+
+def newHinge():
+    me = mesh.newHexahedron(5, 7.5, 15)
+
+    # paperclip diam is 1.2mm
+
+    diam=1.2
+
+    sizes = {5:diam,
+             2.5:diam+0.05,
+             0:diam+0.1,
+             -2.5:diam+0.15,
+             -5:diam+0.4
+             }
+
+    for z in sizes:
+        d = sizes[z]
+        cy = mesh.newCylinder(diameter=d, depth=100)
+        mesh.rotateMesh(cy, YAXIS, 90)
+        mesh.transposeMesh(cy, dz=z)
+        me = mesh.fromDifference(me, cy, True)
+
+
+    return object.newObjectFromMesh("cubey", me)
+    pass
+
+
+# import sys, importlib; sys.path.append('/home/jim/git/blendertools'); from bt.models.hinge import hinge;
+# print('reloading...'); importlib.reload(hinge); print('loaded'); obj = hinge.main()
+
+def main():
+    # This is so we can modify mesh library and see updates in blender
+    # otherwise we have to restart blender each time we make a change to mesh.py
+    import importlib;
+
+    for lib in [mesh, object]:
+        importlib.reload(lib)
+    #
+    #
+    # sm = bpy.data.objects['BezierCurve']
+    # mesh.codeFromObject(sm, "/tmp/tmp.py")
+    # return
+
+    # delete all objects
+    for obj in bpy.data.objects:
+        object.deleteObjIfExists(obj.name)
+
+    return newHinge()
